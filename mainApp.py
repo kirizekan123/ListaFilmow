198b49004f65c2318cb2c298c9602a88791b38a3
kirizekan123 <radek9032@gmail.com>
2025-05-06 12:31:24 +0200

    Add files via upload

---
+++ mainApp.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import yt_dlp
+
+app = Flask(__name__)
+
+@app.route('/get', methods=['GET'])
+def get_video_url():
+    try:
+        url = request.args.get('url')
+        ydl_opts = {
+            'quiet': True,
+            'skip_download': True,
+            'format': 'best[height<=720]/best',
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=False)
+
+        return jsonify({
+            'title': info.get('title'),
+            'url': info.get('url'),
+            'ext': info.get('ext')
+        })
+    except Exception as e:
+        return jsonify({
+            'title': '',
+            'url': '',
+            'ext': '',
+            'error': str(e)
+        })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
